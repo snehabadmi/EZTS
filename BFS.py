c37d274b8b8e0bd2d84e0ecb017bb96db7ba5383
snehabadmi <badmisneha1204@gmail.com>
2024-06-19 20:22:07 +0530

    Add files via upload

---
+++ BFS.py
@@ -0,0 +1,39 @@
+class node:
+    def __init__(self,data):
+        self.value=data
+        self.left=None
+        self.right=None
+def levelorder(root):
+    Q=[root]
+    Q.append(None)
+    
+    while len(Q)>0:
+        curr=Q.pop(0)
+        if curr==None:
+            if len(Q)==0:
+                break
+            else:
+                print()
+                Q.append(None)
+        else:
+            print(curr.value)
+            if curr.left!=None:
+                Q.append(curr.left)
+            if curr.right!=None:
+                Q.append(curr.right)
+if __name__=="__main__":
+        
+        root=node(1)
+        root.left=node(2)
+        root.right=node(3)
+        root.left.left=node(4)
+        root.left.right=node(5)
+        root.right.left=node(6)
+        root.right.right=node(7)
+        root.left.right.left=node(9)
+        root.left.right.right=node(10)
+        root.right.right.right=node(11)
+        root.left.right.left.left=node(12)
+        root.left.right.left.right=node(13)
+print("levelorder:")
+print(levelorder(root))
