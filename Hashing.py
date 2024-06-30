299fa058f36931739bfbf084c77cae7073920e5a
snehabadmi <badmisneha1204@gmail.com>
2024-06-30 10:44:20 +0530

    Add files via upload

---
+++ Hashing.py
@@ -0,0 +1,19 @@
+# Hashing 
+class node :
+    def __init__(self,data):
+        self.value = data
+        self.next = None
+    def insert_end(self):
+        tail.next=temp
+        tail=temp
+        return
+    def search(self):
+        curr
+        
+    def delete_middle(self):
+        prev.next=curr.next
+        return
+    def hashing(k):
+        for i in range(0,len(k)):
+            H = k % 10
+print(H)
